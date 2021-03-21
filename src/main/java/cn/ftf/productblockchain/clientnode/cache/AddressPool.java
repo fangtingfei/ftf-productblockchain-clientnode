2915e6e95e4e7d3e96697d875b10f22aef99fc44
fangtingfei <1159851944@qq.com>
2021-03-21 18:51:28 +0800

    实现添加节点，移除节点功能

--- src/main/java/cn/ftf/productblockchain/clientnode/cache/AddressPool.java
+++ src/main/java/cn/ftf/productblockchain/clientnode/cache/AddressPool.java
@@ -1,8 +1,11 @@
 package cn.ftf.productblockchain.clientnode.cache;
 
+import cn.ftf.productblockchain.clientnode.websocket.MyClient;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashSet;
 
 /**
@@ -20,8 +23,25 @@ public class AddressPool {
 
     @PostConstruct
     public void init(){
-        addressPoll.add("ws://127.0.0.1:7000");
-        addressPoll.add("ws://127.0.0.1:7001");
-        addressPoll.add("ws://127.0.0.1:7002");
+        //初始化客户端
+//        addressPoll.add("ws://127.0.0.1:7000");
+//        addressPoll.add("ws://127.0.0.1:7001");
+//        addressPoll.add("ws://127.0.0.1:7002");
+    }
+
+    public void addURL(String url) throws URISyntaxException {
+        addressPoll.add(url);
+        MyClient client = new MyClient(new URI(url));
+        client.connect();
+    }
+    public void removeURL(String url){
+        addressPoll.remove(url);
+    }
+    public HashSet<String> getAddressPoll() {
+        return addressPoll;
+    }
+
+    public void setAddressPoll(HashSet<String> addressPoll) {
+        AddressPool.addressPoll = addressPoll;
     }
 }
--- src/main/java/cn/ftf/productblockchain/clientnode/controller/WebSocketController.java
+++ src/main/java/cn/ftf/productblockchain/clientnode/controller/WebSocketController.java
@@ -1,7 +1,13 @@
 package cn.ftf.productblockchain.clientnode.controller;
 
+import cn.ftf.productblockchain.clientnode.cache.AddressPool;
 import cn.ftf.productblockchain.clientnode.message.Result;
+import org.mockito.internal.junit.UniversalTestListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.annotation.PostConstruct;
@@ -14,11 +20,21 @@ import javax.annotation.PostConstruct;
  */
 @Controller
 public class WebSocketController {
-
+    Logger logger= LoggerFactory.getLogger(getClass());
+    @Autowired
+    private AddressPool addressPool;
     @PostMapping
-    public Result addNode(String url){
-
+    public Result addNode (String url)throws Exception{
+        addressPool.addURL(url);
+        logger.info("[添加节点成功] url={}",url);
         return new Result(true,"success");
     }
+    @PostMapping
+    public Result removeNode (String url)throws Exception{
+        addressPool.removeURL(url);
+        logger.info("[移除节点成功] url={}",url);
+        return new Result(true,"success");
+    }
+
 
 }
